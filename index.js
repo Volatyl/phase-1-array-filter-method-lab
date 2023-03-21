d0b7ee5b47beb44573c7673606ed2a9bf14300b1
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-21 21:22:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(array, name) {
+  return array.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(array, string) {
+  return array.filter((n) => n.slice(0, string.length) === string);
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => driver.name === name);
+}
